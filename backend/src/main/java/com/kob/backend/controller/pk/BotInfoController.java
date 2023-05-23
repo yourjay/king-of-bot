63cbbae0531bf3615acc86b54ef2ea23bc4b90d9
林 愉悦 <2606610384@qq.com>
2023-05-23 16:56:15 +0800

    第一次修改

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,12 +3,19 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public String getBotInfo()
+    public Map<String,String> getBotInfo()
     {
-        return "hhhhhh";
+        Map<String,String> bot=new HashMap<>();
+        bot.put("name","tyty");
+        bot.put("rating","1500");
+
+        return bot;
     }
 }
