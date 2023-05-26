5d96804092cd5716b7fde831c5ede4e07228dd85
林 愉悦 <2606610384@qq.com>
2023-05-26 18:41:44 +0800

    完善游戏界面

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -13,18 +13,101 @@ export class GameMap extends AcGameObject{
         this.rows=13;
         this.cols=13;
 
+
+
+        this.inner_walls_count=20;
          this.walls=[];
 
     }
+
+    check_connectivity(g,sx,sy,tx,ty){
+        if(sx==tx&&sy==ty) return true;
+
+        g[sx][sy]=true;
+
+        let dx=[-1,0,1,0],dy=[0,1,0,-1];
+        for(let i=0;i<4;i++){
+            let x=sx+dx[i],y=sy+dy[i];
+            if(!g[x][y]&&this.check_connectivity(g,x,y,tx,ty))
+            {
+                return true;
+            }
+                
+        }
+        return false;
+
+    }
     create_walls(){
-            new Wall(1,1,this)
+            const g=[];
+            for(let r=0;r<this.rows;r++)
+            {
+                g[r]=[];
+                for(let c=0;c<this.cols;c++)
+                {
+                    g[r][c]=false;
+                }
+            }
+            //给四周的障碍物加上
+            for(let r=0;r<this.rows;r++)
+            {
+                g[r][0]=g[r][this.cols-1]=true;
+            }
+            for(let c=0;c<this.cols;c++)
+            {
+                g[0][c]=g[this.rows-1][c]=true;
+            }
+
+            //创建随机障碍物
+            for(let i=0;i<this.inner_walls_count/2;i++)
+            {
+                for(let j=0;j<1000;j++)
+                {
+                    let r=parseInt(Math.random()*this.rows);
+                    let c=parseInt(Math.random()*this.cols);
+                    if(g[r][c]||g[c][r]) continue;
+                    if(r==this.rows-2&&c==1||r==1&&c==this.cols-2)
+                    {
+                        continue;
+                    }
+                    g[r][c]=g[c][r]=true;
+                        break;
+                }
+
+            }
+            const copy_g=JSON.parse(JSON.stringify(g));
+
+            if(!this.check_connectivity(copy_g,this.rows-2,1,1,this.cols-2)) 
+            {
+                return false;
+            }
+
+
+            for(let r=0;r<this.rows;r++)
+            {
+                for(let c=0;c<this.cols;c++)
+                {
+                    if(g[r][c]){
+                        this.walls.push(new Wall(r,c,this));
+                    }
+                }
+            }
+            return true;
     }
     start(){
-        this.create_walls();
+        for(let i=0;i<1000;i++)
+        {
+            if(this.create_walls())
+            {
+                break;
+            }
+         
+
+        }
+           
     }
 
     update_size(){
-            this.L=Math.min(this.parent.clientWidth/this.cols,this.parent.clientHeight/this.rows);
+            this.L=parseInt(Math.min(this.parent.clientWidth/this.cols,this.parent.clientHeight/this.rows));
 
             this.ctx.canvas.width=this.L*this.cols;
             this.ctx.canvas.height=this.L*this.rows;
--- web/src/components/PlayGround.vue
+++ web/src/components/PlayGround.vue
@@ -24,7 +24,6 @@ export default{
 div.playground{
     width: 60vw;
     height: 70vh;
-    background: lightblue;
     margin: 40px auto;
 }
 </style>
